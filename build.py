9075a14e9f59c7ee3d41aa9f9097aa7fd1809422
Alexis Lopez Zubieta <contact@azubieta.net>
2019-06-13 16:35:44 -0500

    add conan comunity repo

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     remotes = [("https://api.bintray.com/conan/bincrafters/public-conan", "yes", "bincrafters"),
+               ("https://api.bintray.com/conan/conan-community/conan", "yes", "conan-community"),
                ("https://api.bintray.com/conan/appimage-conan-community/public-conan", "yes", "appimage")]
 
     builder = ConanMultiPackager(build_policy="outdated", remotes=remotes)
